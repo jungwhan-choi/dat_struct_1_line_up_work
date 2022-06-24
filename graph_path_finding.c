1ee150a846f4d3a653bd52cdb0c00ad14ea0098c
LAPTOP-NPA8R8SG\zad34 <zad3415@naver.com>
2022-06-24 17:17:48 +0900

    adding_list_maker(runtime error occur)

--- graph_path_finding.c
+++ graph_path_finding.c
@@ -1,8 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define max_vertex 12
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+
 typedef struct listnode* listpointer;
 typedef struct listnode {
 	int num;
@@ -12,27 +13,27 @@ typedef struct listnode {
 double cost[max_vertex][max_vertex];
 listpointer graph[max_vertex];
 int distance, pred, set_S;
-void read_and_make_graph() {
-	cost_maker();
-	
+
+void makenode(listpointer* p, int n) {
+	listpointer new;
+	new = (listpointer)malloc(sizeof(listnode));
+	(*p)->link = new;
+	new->num = n;
+	new->link = NULL;
 }
+
 void cost_maker() {
 	FILE* fp = fopen("graphdata.txt", "r");
 	for (int i = 0; i < max_vertex; i++) {
-		int vt = 0, next_vt = 0;
-		fscanf(fp, "%d", &vt);
-		printf("\n");
+		int vt = 0, next_vt = 0, e = 0;
 		char v = NULL;
 		double value = 0;
-		int e = 0;
+		fscanf(fp, "%d", &vt);
 		do {
 			fscanf(fp, "%d", &next_vt);
 			fscanf(fp, "%lf", &value);
 			e = fscanf(fp, "%c", &v);
 			cost[vt][next_vt] = value;
-
-
-			printf("(%d,%d):%.1lf\n", vt, next_vt, cost[vt][next_vt]);
 			if (v == '\n' || e == EOF) {
 				break;
 			}
@@ -42,32 +43,49 @@ void cost_maker() {
 		}
 	}
 }
-void list_maker() {
 
+void list_maker() {
+	FILE* fp = fopen("graphdata.txt", "r");
+	for (int i = 0; i < max_vertex; i++) {
+		int vt = 0, next_vt = 0, e = 0;
+		char v = NULL;
+		double value = 0;
+		fscanf(fp, "%d", &vt);
+		listpointer temp = graph[vt];
+		do {
+			fscanf(fp, "%d", &next_vt);
+			fscanf(fp, "%lf", &value);
+			e = fscanf(fp, "%c", &v);
+			makenode(&temp, next_vt);
+			temp = temp->link;
+			if (v == '\n' || e == EOF) {
+				break;
+			}
+		} while (1);
+		if (e == EOF) {
+			break;
+		}
+	}
 }
-void makenode(listpointer* p,int n) {
-	listpointer new;
-	new = (listpointer)malloc(sizeof(listnode));
-	(*p)->link = new;
-	new->num = n;
-	new->link = NULL;
+
+void read_and_make_graph() {
+	cost_maker();
+	list_maker();
 }
+
+
 int shortest_path (int start,int dest){
 }
 int main() {
-	
-	for (int i = 0; i < max_vertex; i++) {
-		for (int j = 0; j < max_vertex; j++) {
-			cost[i][j] = -1;
-		}
-		
-	}
 	read_and_make_graph();
 	for (int i = 0; i < max_vertex; i++) {
-		for (int j = 0; j < max_vertex; j++) {
-			printf("%.1lf  ", cost[i][j]);
-		}
-		printf("\n");
+		listpointer temp;
+		temp = graph[i];
+		do {
+			printf("%d -> ", temp->num);
+			temp = temp->link;
 
+		} while (temp->link != NULL);
+		printf("\n");
 	}
 }
